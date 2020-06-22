68057999605c1e1bfc1558da74f3aa263264c3d6
rishabhjaincoder <rishabhjain.rdps@gmail.com>
2020-06-23 03:13:10 +0530

    Configured React to use Reactstrap and bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// here we have imported bootstrap in our application
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
