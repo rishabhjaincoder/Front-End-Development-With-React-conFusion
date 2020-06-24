939416c01cf5a4ba09a248639cc494c5e4be1c6a
rishabhjaincoder <rishabhjain.rdps@gmail.com>
2020-06-25 01:32:34 +0530

    added Menu Component after Nav

--- src/App.js
+++ src/App.js
@@ -3,30 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 // importing navbar from reactstrap
 import { Navbar, NavbarBrand } from 'reactstrap';
-
-// this is function based component
-// function App() {
-//   return (
-//     <div className="App">
-//         <Navbar dark color="primary">
-//           <div className="container">
-//             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-//           </div>
-//         </Navbar>
-//       </div>
-//   );
-// }
+// importing menu from the components
+import Menu from './components/MenuComponent';
 
 // this is class based component
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante ConFusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
     );
   }
