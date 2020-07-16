f645b6dd9ebcb088d053843b4050099d583b1714
rishabhjaincoder <rishabhjain.rdps@gmail.com>
2020-07-17 01:15:08 +0530

    fetch - handling errors

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -16,6 +16,7 @@ export const addComment = (dishId, rating, author, comment) => ({
 
 // creating this as a thunk, therefore this returns a function instead of an object
 // inner function in thunk gets access to dispatch as well as getState function
+
 export const fetchDishes = () => (dispatch) => {
 
     dispatch(dishesLoading(true));
@@ -24,9 +25,24 @@ export const fetchDishes = () => (dispatch) => {
     // setTimeout(() => {
     //     dispatch(addDishes(DISHES));
     // }, 2000);
+
     return fetch(baseUrl + 'dishes')
+        .then(response => {
+            if (response.ok) {
+                return response;
+            } else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        },
+            error => {
+                var errmess = new Error(error.message);
+                throw errmess;
+            })
         .then(response => response.json())
-        .then(dishes => dispatch(addDishes(dishes)));
+        .then(dishes => dispatch(addDishes(dishes)))
+        .catch(error => dispatch(dishesFailed(error.message)));
 }
 
 export const dishesLoading = () => ({
@@ -46,8 +62,22 @@ export const addDishes = (dishes) => ({
 // this is a redux thunk, coz it returns a function
 export const fetchComments = () => (dispatch) => {
     return fetch(baseUrl + 'comments')
+        .then(response => {
+            if (response.ok) {
+                return response;
+            } else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        },
+            error => {
+                var errmess = new Error(error.message);
+                throw errmess;
+            })
         .then(response => response.json())
-        .then(comments => dispatch(addComments(comments)));
+        .then(comments => dispatch(addComments(comments)))
+        .catch(error => dispatch(commentsFailed(error.message)));
 };
 
 export const commentsFailed = (errmess) => ({
@@ -66,8 +96,22 @@ export const fetchPromos = () => (dispatch) => {
     dispatch(promosLoading());
 
     return fetch(baseUrl + 'promotions')
+        .then(response => {
+            if (response.ok) {
+                return response;
+            } else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        },
+            error => {
+                var errmess = new Error(error.message);
+                throw errmess;
+            })
         .then(response => response.json())
-        .then(promos => dispatch(addPromos(promos)));
+        .then(promos => dispatch(addPromos(promos)))
+        .catch(error => dispatch(promosFailed(error.message)));
 }
 
 export const promosLoading = () => ({
