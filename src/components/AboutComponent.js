9345c3bfe558f2314bc74ba634299dee98684a2f
rishabhjaincoder <rishabhjain.rdps@gmail.com>
2020-07-18 02:12:45 +0530

    Removed unnecessary code

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -3,7 +3,7 @@ import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'r
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
 import { Link } from 'react-router-dom';
-import { FadeTransform, Fade, Stagger } from 'react-animation-components';
+import { Fade, Stagger } from 'react-animation-components';
 
 function RenderLeader({ leader, isLoading, errMess }) {
     if (isLoading) {
